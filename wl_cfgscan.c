7c8910a50f95d97b31d74faa78d9d3d4d9d0a91b
Damon Kim <taekhun.kim@broadcom.corp-partner.google.com>
2021-10-21 18:32:38 +0900

    bcmdhd: Added debug code to find out the root cause of scan timeout issue
    Scan timeout issue has been reported intermittently, but the root cause has not been found yet.
    A something in common is that sync_id in event message indicates ‘0’,
    it resulted in scan timeout since scan timer didn’t clear due to sync_id mismatch.
    
    So, we should take a closer look at the moment the FW send up a scan completion event with 'sync_id 0'.
    We're planning to debug like below. If hit the trap, we can find out more why sync_id 0 event delivers.
    - DHD: To make sure DHD driver doesn't provide sync_id 0, we'll modify to start from sync_id 1
    - FW: Add assert trap in FW in case of sync_id 0 during scan completion context
    
    Bug: 202297336
    Test: Confirmed that it works well even if scan sync_id starts with '1'.
    Change-Id: I2468b0f3ca105117a8797662d5b4f118ff779a6e
    Merged-In: I2468b0f3ca105117a8797662d5b4f118ff779a6e
    Signed-off-by: Roger Wang <wangroger@google.com>

--- wl_cfgscan.c
+++ wl_cfgscan.c
@@ -189,7 +189,11 @@ wl_escan_check_sync_id(struct bcm_cfg80211 *cfg, s32 status, u16 result_id, u16 
 	}
 }
 #define wl_escan_increment_sync_id(a, b) ((a)->escan_info.cur_sync_id += b)
+#ifdef SYNCID_MISMATCH_DEBUG
+#define wl_escan_init_sync_id(a) ((a)->escan_info.cur_sync_id = 1)
+#else
 #define wl_escan_init_sync_id(a) ((a)->escan_info.cur_sync_id = 0)
+#endif /* SYNCID_MISMATCH_DEBUG */
 #else
 #define wl_escan_get_buf(a, b) ((wl_scan_results_t *) (a)->escan_info.escan_buf)
 #define wl_escan_check_sync_id(a, b, c, d) 0
