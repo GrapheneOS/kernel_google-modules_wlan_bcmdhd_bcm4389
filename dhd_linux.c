abebe192d48c7d456d0f4e41226267993c6dbabe
Damon Kim <taekhun.kim@broadcom.corp-partner.google.com>
2021-11-11 20:50:26 +0900

    bcmdhd: Fixed to use vmalloc instead of kmalloc for debug module
    Google reported several KP issues by memory allocation failure during attach/detach flow.
    Basically, KP is known issue, it can be handled in pa/2089102.
    For debug module memory, kmalloc changed to vmalloc to reduce memory allocation failure.
    Also, error handling & logging enhanced to identify failure points.
    
    Bug: 205228870, 205683907
    Test: No issue is seen in basic sanity test
    Change-Id: Ice4bd603166927e1b2003bcb4b292724e31e77c9
    Merged-In: Ice4bd603166927e1b2003bcb4b292724e31e77c9
    Signed-off-by: Roger Wang <wangroger@google.com>
    (cherry picked from commit b778950931fefc993a610a97c6a00b15e012a39e)

--- dhd_linux.c
+++ dhd_linux.c
@@ -4011,8 +4011,8 @@ dhd_cancel_logtrace_process_sync(dhd_info_t *dhd)
 #ifdef DHD_USE_KTHREAD_FOR_LOGTRACE
 	tsk_ctl_t *tsk = &dhd->thr_logtrace_ctl;
 
-	if (tsk->parent && dhd->thr_logtrace_ctl.thr_pid >= 0) {
-		PROC_STOP_USING_BINARY_SEMA(&dhd->thr_logtrace_ctl);
+	if (tsk->parent && tsk->thr_pid >= 0) {
+		PROC_STOP_USING_BINARY_SEMA(tsk);
 	} else {
 		DHD_ERROR(("%s: thr_logtrace_ctl(%ld) not inited\n",
 			__FUNCTION__, tsk->thr_pid));
